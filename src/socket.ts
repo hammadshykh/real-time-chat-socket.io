51e3db8b360575520435431f5071ab7e38252f2a
sheikhmubashi <hammadyounus.508@gmail.com>
2025-08-12 17:12:27 +0500

    update: code

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,10 @@
 "use client";
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 // Create one socket instance for the entire app
-export const socket = io();
+export const socket: Socket = io(
+ process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000",
+ {
+  autoConnect: false, // we will manually connect to control lifecycle
+ }
+);

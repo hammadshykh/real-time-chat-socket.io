a8fe81f693b7e6ab88c4d017d19764e8fbd743b7
sheikhmubashi <hammadyounus.508@gmail.com>
2025-08-12 17:24:48 +0500

    remove: server.js

--- src/socket.ts
+++ src/socket.ts
@@ -3,7 +3,7 @@ import { io, Socket } from "socket.io-client";
 
 // Create one socket instance for the entire app
 export const socket: Socket = io(
- process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000",
+ process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:4000",
  {
   autoConnect: false, // we will manually connect to control lifecycle
  }
